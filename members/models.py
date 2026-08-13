2fb679ec700826a102a49b804a54140c4c1779bd
nothing36 <prodipsarker953@gmail.com>
2026-08-13 10:29:16 +0600

    database model added

--- members/models.py
+++ members/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Member (models.Model):
     firstName=models.CharField(max_length=255)
     secondName=models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
